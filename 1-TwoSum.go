1ce9f6c473ef206574eae315cb3d8be86a9d413d
antonio <antonio.rl.1011@gmail.com>
2024-05-27 20:25:31 +0200

    Added better solution to twosum and added palindrome

--- 1-TwoSum.go
+++ 1-TwoSum.go
@@ -11,3 +11,19 @@ func twoSum(nums []int, target int) []int {
 
 	return nil
 }
+
+func twoSumImproved(nums []int, target int) []int {
+	numMap := make(map[int]int)
+
+	for idx, num := range nums {
+		complement := target - num
+
+		if idx2, ok := numMap[complement]; ok {
+			return []int{idx2, idx}
+		}
+
+		numMap[num] = idx
+	}
+
+	return nil
+}
